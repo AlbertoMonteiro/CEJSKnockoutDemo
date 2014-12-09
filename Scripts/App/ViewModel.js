028edb01c7d3174332f30a6e6f6bca9d55c45cd2
Alberto Monteiro <alberto.monteiro@live.com>
2014-12-09 10:02:46 -0300

    initial commit

---
+++ Scripts/App/ViewModel.js
@@ -0,0 +1,21 @@
+﻿function PessoaViewModel(nome, idade) {
+    this.nome = ko.observable(nome);
+    this.idade = ko.observable(idade);
+}
+
+function ViewModel() {
+    this.pessoas = ko.observableArray([new PessoaViewModel("Alberto", 24)]);
+    this.pessoa = new PessoaViewModel();
+    this.nomeFocado = ko.observable(false);
+
+    this.totalPessoas = ko.computed(function () {
+        return this.pessoas().length;
+    }, this);
+
+    this.adicionarPessoa = function () {
+        this.pessoas.push(new PessoaViewModel(this.pessoa.nome(), this.pessoa.idade()));
+        this.pessoa.nome("");
+        this.pessoa.idade("");
+        this.nomeFocado(true);
+    };
+}
---
+++ Scripts/Components/Grid/Grid.js
@@ -0,0 +1,14 @@
+﻿$.ajax("/Scripts/Components/Grid/Template.html", {
+    async: false,
+    success: function (html) {
+        ko.components.register('grid', {
+            viewModel: function (params) {
+                this.colunas = Object.getOwnPropertyNames(params.items()[0]).map(function (item) {
+                    return { valor: item, exibicao: item.charAt(0).toUpperCase() + item.slice(1) };
+                });
+                this.items = params.items;
+            },
+            template: html
+        });
+    }
+});
